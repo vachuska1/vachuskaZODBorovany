32715390a152a6e7054a86d91af7aec87701f2b3
vachuska1 <vachuska1@github.com>
2025-07-10 10:00:36 +0200

    neverejny pristup na admin page

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,32 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/admin/:path*', // Blokuje vše pod /admin
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex, nofollow, noarchive'
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-store'
+          }
+        ]
+      },
+      {
+        source: '/api/:path*', // Blokuje i API routes
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex, nofollow'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 export default nextConfig
